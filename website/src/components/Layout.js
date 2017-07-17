652ebf700ea3e6eb059ed8adbfccd3e823619564
LDoolittle <LDoolittle@musicnotes.com>
2017-07-17 15:28:47 -0500

    added new logo and different icons

--- website/src/components/Layout.js
+++ website/src/components/Layout.js
@@ -8,9 +8,7 @@ export default class Layout extends React.Component {
     return (
       <div className="app-container">
         <header>
-          <Link to="/">
             <img className="logo" src="/img/logo.jpg"/>
-          </Link>
         </header>
         <div className="app-content">{this.props.children}</div>
         <footer>
