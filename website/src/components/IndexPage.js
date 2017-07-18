898e03751f80812fd32dec385697d32739548e7f
LDoolittle <LDoolittle@musicnotes.com>
2017-07-18 10:36:36 -0500

    added some new movies for testing purposes

--- website/src/components/IndexPage.js
+++ website/src/components/IndexPage.js
@@ -11,7 +11,7 @@ export default class IndexPage extends React.Component {
 
       this.state = {
         ratings:{someMovie:1},
-        recommendations:[{movie_id:1, rating: 10}, {movie_id:2, rating:0}]
+        recommendations:[{movie_id:6, rating: 10}]
       };
 
       this.onStarClick = this.onStarClick.bind(this);
@@ -29,7 +29,7 @@ export default class IndexPage extends React.Component {
     this.forceUpdate();
     if(Object.keys(this.state.ratings).length > 2) {
       //fetch('/api').then(this.onRecosReceived);
-      this.onRecosReceived([{movie_id: 1, rating: 97}, {movie_id: 2, rating: 80}, {movie_id: 3, rating: 99}, {movie_id: 4, rating: 40}, {movie_id: 5, rating: 2}])
+      this.onRecosReceived([{movie_id:6, rating:99}, {movie_id:7, rating:90}, {movie_id:8, rating:85}, {movie_id:9, rating:82}, {movie_id:10, rating:79}])
     }
   }
 
@@ -39,7 +39,7 @@ export default class IndexPage extends React.Component {
       <div className="home">
         <div className="movies-selector">
           <div className="category-header">ratings</div>
-          {movies.map(movieData => <MovieRating key={movieData.id} {...{data: movieData, onStarClick: this.onStarClick} } />)}
+          {movies.slice(0,5).map(movieData => <MovieRating key={movieData.id} {...{data: movieData, onStarClick: this.onStarClick} } />)}
         </div>
         <div className="movies-selector">
           <div className="category-header">recommendations</div>
