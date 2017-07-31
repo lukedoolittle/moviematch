191bded5c225cd94edfb835fb423f1598e562dbc
lukedoolittle <luke.doolittle@gmail.com>
2017-07-31 15:13:15 -0500

    added a loading spinner while waiting for recommendations

--- webserver/src/components/IndexPage.js
+++ webserver/src/components/IndexPage.js
@@ -3,16 +3,16 @@
 import React from 'react';
 import MovieRating from './MovieRating';
 import MovieReco from './MovieReco';
-import TransitionGroup from 'react-transition-group/TransitionGroup';
-import CSSTransition from 'react-transition-group/CSSTransition';
+import Loadable from 'react-loading-overlay'
 
 export default class IndexPage extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      ratings:[],
-      recommendations:[]
+      ratings: [],
+      recommendations: [],
+      loading: false
     };
     this.onStarClick = this.onStarClick.bind(this);
     this.onRecosReceived = this.onRecosReceived.bind(this);
@@ -24,24 +24,29 @@ export default class IndexPage extends React.Component {
       .then(result => {
         this.setState({movies: result,
                        ratings: this.state.ratings, 
-                       recommendations: this.state.recommendations});
+                       recommendations: this.state.recommendations,
+                       loading: this.state.loading});
     });
   }
 
   onRecosReceived(recommendations){
     this.setState({movies: this.state.movies,
                    ratings: this.state.ratings, 
-                   recommendations: recommendations});
-    //console.log('I was triggered during fetch callback')
+                   recommendations: recommendations,
+                   loading: false});
   }
 
   onStarClick(nextValue, prevValue, name) {
     this.state.ratings.push({movie_id: name, rating: nextValue})
     this.setState({movies: this.state.movies,
                    ratings: this.state.ratings, 
-                   recommendations: this.state.recommendations});
-    console.log(this.state.ratings)
+                   recommendations: this.state.recommendations,
+                   loading: this.state.loading});
     if(this.state.ratings.length === 5) {
+      this.setState({movies: this.state.movies,
+                     ratings: this.state.ratings, 
+                     recommendations: this.state.recommendations,
+                     loading: true});
       fetch('/api/predict',
         {
           headers: {
@@ -59,20 +64,21 @@ export default class IndexPage extends React.Component {
   render() {
     const {recommendations} = this.state;
     const items = this.state.movies.map(movieData => 
-      <CSSTransition
-          key={movieData.movie_id}
-          classNames="example"
-          timeout={{ enter: 500, exit: 300 }}>
         <MovieRating key={movieData.movie_id} {...{data: movieData, onStarClick: this.onStarClick} }/>
-      </CSSTransition>
     );
     return (
       <div className="home">
         <div className="movies-selector">
           <div className="category-header">ratings</div>
-          <TransitionGroup>
-            {items}
-          </TransitionGroup>
+          <div className="movies-ratings">
+            <Loadable
+              active={this.state.loading}
+              spinner={true}
+              animate={true}
+              text='Generating recommendations...'>
+              {items}
+            </Loadable>
+          </div>
         </div>
         <div className="movies-selector">
           <div className="category-header">recommendations</div>
